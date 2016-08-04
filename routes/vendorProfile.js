6ff34996418eb0377bfb706e7938337a9ce5878d
o000o <ooonaja@gmail.com>
2016-08-04 13:06:10 +0700

    Ta vendorProfile Done again

--- routes/vendorProfile.js
+++ routes/vendorProfile.js
@@ -1,207 +1,217 @@
 'use strict'
-
-const logger = require('../utils/logUtils');
-const async = require('async');
-const resp = require('../utils/respUtils');
-const util = require('../utils/bmsUtils');
-const jsUtil = require('util');
-const error = require('../config/error');
-const cst = require('../config/constant');
-const mVendorProfile = require('../models/mVendorProfile');
-const mVendorContact = require('../models/mVendorProfileContact');
-
+const logger = require('../utils/logUtils')
+const async = require('async')
+const resp = require('../utils/respUtils')
+const util = require('../utils/bmsUtils')
+const jsUtil = require('util')
+const error = require('../config/error')
+const cst = require('../config/constant')
+const mVendorProfile = require('../models/mVendorProfile')
+const mVendorContact = require('../models/mVendorProfileContact')
 const vendorProfile = {
-
-  add: (req, res) => {
-	let cmd = 'addVendorProfile';
-	try{	
-		let jWhere = {vendorType: req.body.requestData.vendorType, vendorName1: req.body.requestData.vendorName1};
-		logger.info(req,cmd+'|where:'+JSON.stringify(jWhere));
-		mVendorProfile.findOrCreate({where:jWhere, 
-			defaults:req.body.requestData,
-  			include: [{model: mVendorContact, as:'vendorContactList'}]
-		}).spread((db,succeed) => {
-			logger.info(req,cmd+'|add VendorProfile complete');
-  			return resp.getSuccess(req,res,cmd,db);
-		}).catch((err) => {
-    			logger.error(req,cmd+'|Error when create mVendorProfile|'+err);
-    			logger.summary(req,cmd+'|'+error.desc_01001);
-    			res.json(resp.getJsonError(error.code_01001,error.desc_01001,err));
-		})
-	}catch(err){                          
-      		logger.error(req,cmd+'|'+err);
-      		return resp.getInternalError(req,res,cmd,err);
-    	}  
-  },
-
-  edit: (req, res) => {
-        let cmd = 'updateVendorProfile';
-        try{
-                let jWhere = {vendorId: req.body.requestData.vendorId};
-      		logger.info(req,cmd+'|where:'+JSON.stringify(jWhere));
-                mVendorProfile.update(req.body.requestData, {where:jWhere}).then((succeed) => {
-			logger.info(req,cmd+'|update VendorProfile complete');
-
-			let asyncTasks = [];
-			if(util.isDataFound(req.body.requestData.vendorContactList)){
-			req.body.requestData.vendorContactList.forEach(function(vendorContact) {
-				let JWhere = {};
-       				cmd = 'updateVendorContact';
-				if(vendorContact.editAction == "E"){
-					vendorContact.vendorId = req.body.requestData.vendorId;
-					JWhere = {vendorContactId: vendorContact.vendorContactId};
-      					logger.info(req,cmd+'|where:'+JSON.stringify(JWhere));
-					delete vendorContact.editAction;
-                			asyncTasks.push(function(callback){
-						mVendorContact.update(vendorContact, {where:JWhere}).then((succeed) => {
-                                                	logger.info(req,cmd+'|update VendorContact complete');
-							callback(null,succeed);
-                                        	}).catch((err) => {
-                                                	logger.error(req,cmd+'|Error when update mVendorContact|'+err);
-							callback(err,null);
-                                        	})
-					});
-				}else if(vendorContact.editAction == "A"){
-					vendorContact.vendorId = req.body.requestData.vendorId;	
-					delete vendorContact.editAction;
-      					logger.info(req,cmd+'|Add:'+JSON.stringify(vendorContact));
-					asyncTasks.push(function(callback){
-						mVendorContact.create(vendorContact).then((succeed) => {
-                                                	logger.info(req,cmd+'|add VendorContact complete');
-							callback(null,succeed);
-                                        	}).catch((err) => {
-                                                	logger.error(req,cmd+'|Error when create mVendorProfile|'+err);
-							callback(err,null);
-                                        	})
-					});
-                		}else if(vendorContact.editAction == "D"){
-					JWhere = {vendorContactId: vendorContact.vendorContactId};
-					delete vendorContact.editAction;
-                			logger.info(req,cmd+'|where:'+JSON.stringify(JWhere));
-                			asyncTasks.push(function(callback){
-						mVendorContact.destroy({where:JWhere}).then((succeed) => {
-                                                	logger.info(req,cmd+'|deleted '+ succeed +' records');
-							callback(null,succeed);
-                                        	}).catch((err) => {
-                                                	logger.error(req,cmd+'|Error while delete mVendorProfile|'+err);
-							callback(err,null);
-                                        	});
-                			});
-				}
-			})
-			}
-
-			if(asyncTasks.length > 0){
-				async.parallel(asyncTasks, function(err,result){
-					if(err){
-                                		logger.summary(req,cmd+'|'+error.desc_01001);
-                        			res.json(resp.getJsonError(error.code_01001,error.desc_01001,err));
-					}else{
-						logger.info(req,cmd+'|result:'+JSON.stringify(result));
-						resp.getSuccess(req,res,cmd);
-					}
-				});
-			}else{
-				resp.getSuccess(req,res,cmd);
-			}
+    add: (req, res) => {
+        let cmd = 'AddVendorProfile'
+        try {
+            let jWhere = {
+                vendorType: req.body.requestData.vendorType,
+                vendorName1: req.body.requestData.vendorName1
+            }
+            mVendorProfile.findOrCreate({
+                where: jWhere,
+                defaults: req.body.requestData,
+                include: [{
+                    model: mVendorContact,
+                    as: 'vendorContactList'
+                }]
+            }).spread((db, succeed) => {
+                logger.info(req, cmd + '|Data:' + JSON.stringify(req.body.requestData) + '|Create complete')
+                return resp.getSuccess(req, res, cmd, db)
+            }).catch((err) => {
+                logger.error(req, cmd + '|Data:' + JSON.stringify(req.body.requestData) + '|Error:' + err)
+                logger.summary(req, cmd + '|' + error.desc_01001)
+                res.json(resp.getJsonError(error.code_01001, error.desc_01001, err))
+            })
+        } catch (err) {
+            logger.error(req, cmd + '|' + err)
+            return resp.getInternalError(req, res, cmd, err)
+        }
+    },
+    edit: (req, res) => {
+        let cmd = 'UpdateVendorProfile'
+        try {
+            let asyncTasks = []            
+            let requestData = JSON.parse(JSON.stringify(req.body.requestData));
+            if (util.isDataFound(requestData.vendorContactList)) {
+                delete requestData.vendorContactList
+            }
+            asyncTasks.push((callback) => {
+                mVendorProfile.update(requestData, {
+                    where: {
+                        vendorId: requestData.vendorId
+                    }
+                }).then((succeed) => {
+                    logger.info(req, 'UpdateVendorProfile|Data:' + JSON.stringify(requestData) + '|Update complete')
+                    callback(null, succeed)
                 }).catch((err) => {
-                        logger.error(req,cmd+'|Error when update mVendorProfile|'+err);
-                        logger.summary(req,cmd+'|'+error.desc_01001);
-                        res.json(resp.getJsonError(error.code_01001,error.desc_01001,err));
+                    logger.error(req, 'UpdateVendorProfile|Data:' + JSON.stringify(requestData) + '|Error:' + err)
+                    callback(err, null)
                 })
-        }catch(err){        
-                logger.error(req,cmd+'|'+err);
-                return resp.getInternalError(req,res,cmd,err);
-        }       
-  },
-
-  delete: (req, res) => {
-	let cmd = 'deleteVendorProfile';
-    	try{
-      		let jWhere = {vendorId:req.params.vendorId};
-      		logger.info(req,cmd+'|where:'+JSON.stringify(jWhere));
-      		cmd = 'destroyVendorProfile';
-      		mVendorProfile.destroy({where:jWhere}).then((succeed) => {
-        		logger.info(req,cmd+'|deleted '+ succeed +' records');
-        		return resp.getSuccess(req,res,cmd);
-      		}).catch((err) => {
-        		logger.error(req,cmd+'|Error while delete VendorProfile|'+err);
-        		logger.summary(req,cmd+'|'+error.desc_01001);
-        		res.json(resp.getJsonError(error.code_01001,error.desc_01001,err));
-      		});
-    	}catch(err){
-      		logger.error(req,cmd+'|'+err);
-      		return resp.getInternalError(req,res,cmd,err);
-    	}
-  },
-
-  queryByCriteria: (req, res) => {
-	let cmd = 'queryVendorProfileByCriteria';
-    	try{
-		cmd = 'chkPaging';
-      		const jLimit={offset: null, limit: null};
-      		if(Object.keys(req.query).length !=0){
-        		cmd = 'chkPageCount';
-        		if(util.isDigit(req.query.page) && util.isDigit(req.query.count)){
-          			jLimit.offset = (req.query.page -1)*req.query.count;
-          			jLimit.limit = parseInt(req.query.count);
-        		}else{
-          			logger.info(req,cmd+'|page or count is wrong format');
-          			return resp.getIncompleteParameter(req,res,cmd);
-        		}
-      		}
-      		logger.info(req,cmd+'|'+JSON.stringify(jLimit));
-
-		let jWhere = {};
-
-        	cmd = 'genWhere';
-        	if(util.isDataFound(req.body.requestData.vendorCriteria)){
-        		logger.info(req,cmd+'|gen vendorCriteria');
-          		jWhere = req.body.requestData.vendorCriteria;
-        	}else{
-          		logger.info(req,cmd+'|default VendorProfile with no criteria');
-        	}
-	        if(util.isDataFound(req.body.requestData.vendorContactCriteria)){
-          		logger.info(req,cmd+'|gen vendorContactCriteria');
-          		req.body.requestData.vendorContactCriteria.model=mVendorContact;
-          		req.body.requestData.vendorContactCriteria.as=cst.models.vendorContacts;
-          		if(!util.isDataFound(req.body.requestData.vendorContactCriteria.attributes)&&JSON.stringify(req.body.requestData.vendorContactCriteria.attributes)!='[]'){
-            			logger.info(req,cmd+'|default vendorContact with no attributes');
-            			req.body.requestData.vendorContactCriteria.attributes={exclude:['vendorId'] };
-          		}else{
-            			logger.info(req,cmd+'|selected vendorContact attributes');
-          		}
-          		jWhere.include = req.body.requestData.vendorContactCriteria;
-        	}else{
-          		logger.info(req,cmd+'|default vendorContact with no criteria');
-          		jWhere.include = [{model:mVendorContact, as:cst.models.vendorContacts,required: false,attributes: { exclude: ['vendorId'] }}];
-        	}
-		//add paging in to jwhere
-        	jWhere.offset = jLimit.offset;
-        	jWhere.limit = jLimit.limit
-		logger.info(req,cmd+'|searchOptions:'+jsUtil.inspect(jWhere, {showHidden: false, depth: null}));
-		cmd = 'findVendorProfile';
-        	mVendorProfile.findAndCountAll(jWhere).then((db) => {
-          		cmd = 'chkVendorProfileData';
-          		logger.info(req,cmd+'|'+JSON.stringify(db));
-          		if(db.count>0){
-            			logger.info(req,cmd+'|Found VendorProfile');
-            			return resp.getSuccess(req,res,cmd,{"totalRecord":db.count, "vendorProfileList":db.rows});
-          		}else{
-            			logger.summary(req,cmd+'|Not Found VendorProfile');
-            			res.json(resp.getJsonError(error.code_01003,error.desc_01003,db));
-          		}
-        	}).catch((err) => {
-          		logger.error(req,cmd+'|Error while check VendorProfile return|'+err);
-          		logger.summary(req,cmd+'|'+error.desc_01002);
-          		res.json(resp.getJsonError(error.code_01002,error.desc_01002,err));
-        	});
-	}catch(err){
-      		logger.error(req,cmd+'|'+err);
-      		return resp.getInternalError(req,res,cmd,err);
-    	}
-  }
-
-};
-
-module.exports = vendorProfile;
+            })
+            logger.info(req, 'UpdateVendorProfile|ReqData:' + JSON.stringify(req.body.requestData))
+            if (util.isDataFound(req.body.requestData.vendorContactList)) {
+                req.body.requestData.vendorContactList.forEach((vendorContact) => {
+                    if (vendorContact.editAction == "E") {
+                        vendorContact.vendorId = req.body.requestData.vendorId
+                        delete vendorContact.editAction
+                        asyncTasks.push((callback) => {
+                            mVendorContact.update(vendorContact, {
+                                where: {
+                                    vendorContactId: vendorContact.vendorContactId
+                                }
+                            }).then((succeed) => {
+                                logger.info(req, 'UpdateVendorContact|Data:' + JSON.stringify(vendorContact) + '|Update complete')
+                                callback(null, succeed)
+                            }).catch((err) => {
+                                logger.error(req, 'UpdateVendorContact|Error:' + err)
+                                callback(err, null)
+                            })
+                        })
+                    } else if (vendorContact.editAction == "A") {
+                        vendorContact.vendorId = req.body.requestData.vendorId
+                        delete vendorContact.editAction
+                        asyncTasks.push((callback) => {
+                            mVendorContact.create(vendorContact).then((succeed) => {
+                                logger.info(req, 'UpdateVendorContact|Data:' + JSON.stringify(vendorContact) + '|Create complete')
+                                callback(null, succeed)
+                            }).catch((err) => {
+                                logger.error(req, 'UpdateVendorContact|Error:' + err)
+                                callback(err, null)
+                            })
+                        })
+                    } else if (vendorContact.editAction == "D") {
+                        delete vendorContact.editAction
+                        logger.info(req, cmd + '|where:' + JSON.stringify(JWhere))
+                        asyncTasks.push((callback) => {
+                            mVendorContact.destroy({
+                                where: {
+                                    vendorContactId: vendorContact.vendorContactId
+                                }
+                            }).then((succeed) => {
+                                logger.info(req, 'UpdateVendorContact|Data:' + JSON.stringify(vendorContact) + '|Deleted ' + succeed + ' records')
+                                callback(null, succeed)
+                            }).catch((err) => {
+                                logger.error(req, 'UpdateVendorContact|Error:' + err)
+                                callback(err, null)
+                            })
+                        })
+                    }
+                })
+            }
+            async.parallel(asyncTasks, (err, result) => {
+                if (err) {
+                    logger.summary(req, cmd + '|' + error.desc_01001)
+                    res.json(resp.getJsonError(error.code_01001, error.desc_01001, err))
+                } else {
+                    resp.getSuccess(req, res, cmd)
+                }
+            })
+        } catch (err) {
+            logger.error(req, cmd + '|' + err)
+            return resp.getInternalError(req, res, cmd, err)
+        }
+    },
+    delete: (req, res) => {
+        let cmd = 'DeleteVendorProfile'
+        try {
+            mVendorProfile.destroy({
+                where: {
+                    vendorId: req.params.vendorId
+                }
+            }).then((succeed) => {
+                logger.info(req, cmd + '|Data:\"vendorId\": \"' + req.params.vendorId + '\"|Deleted ' + succeed + ' records')
+                return resp.getSuccess(req, res, cmd)
+            }).catch((err) => {
+                logger.error(req, cmd + '|Data:\"vendorId\": \"' + req.params.vendorId + '\"|Error:' + err)
+                logger.summary(req, cmd + '|' + error.desc_01001)
+                res.json(resp.getJsonError(error.code_01001, error.desc_01001, err))
+            })
+        } catch (err) {
+            logger.error(req, cmd + '|' + err)
+            return resp.getInternalError(req, res, cmd, err)
+        }
+    },
+    queryByCriteria: (req, res) => {
+        let cmd = 'QueryVendorProfileByCriteria'
+        try {
+            cmd = 'ChkPaging'
+            const jLimit = {
+                offset: null,
+                limit: null
+            }
+            if (Object.keys(req.query).length != 0) {
+                cmd = 'ChkPageCount'
+                if (util.isDigit(req.query.page) && util.isDigit(req.query.count)) {
+                    jLimit.offset = (req.query.page - 1) * req.query.count
+                    jLimit.limit = parseInt(req.query.count)
+                    logger.info(req, cmd + '|PageCount:' + JSON.stringify(jLimit))
+                } else {
+                    logger.error(req, cmd + '|Error:page or count is wrong format')
+                    return resp.getIncompleteParameter(req, res, cmd)
+                }
+            }
+            let jWhere = {}
+            cmd = 'GenCriteria'
+            if (util.isDataFound(req.body.requestData.vendorCriteria)) {
+                jWhere = req.body.requestData.vendorCriteria
+            }
+            if (util.isDataFound(req.body.requestData.vendorContactCriteria)) {
+                req.body.requestData.vendorContactCriteria.model = mVendorContact
+                req.body.requestData.vendorContactCriteria.as = cst.models.vendorContacts
+                if (!util.isDataFound(req.body.requestData.vendorContactCriteria.attributes) && JSON.stringify(req.body.requestData.vendorContactCriteria.attributes) != '[]') {
+                    req.body.requestData.vendorContactCriteria.attributes = {
+                        exclude: ['vendorId']
+                    }
+                }
+                jWhere.include = req.body.requestData.vendorContactCriteria
+            } else {
+                jWhere.include = [{
+                    model: mVendorContact,
+                    as: cst.models.vendorContacts,
+                    required: false,
+                    attributes: {
+                        exclude: ['vendorId']
+                    }
+                }]
+            }
+            //add paging in to jwhere
+            jWhere.offset = jLimit.offset
+            jWhere.limit = jLimit.limit
+            logger.info(req, cmd + '|AllCriteria:' + jsUtil.inspect(jWhere, {
+                showHidden: false,
+                depth: null
+            }))
+            cmd = 'FindVendorProfile'
+            mVendorProfile.findAndCountAll(jWhere).then((db) => {
+                cmd = 'ChkVendorProfileData'
+                logger.query(req, cmd + '|' + JSON.stringify(db))
+                if (db.count > 0) {
+                    return resp.getSuccess(req, res, cmd, {
+                        "totalRecord": db.count,
+                        "vendorProfileList": db.rows
+                    })
+                } else {
+                    logger.summary(req, cmd + '|Not Found VendorProfile')
+                    res.json(resp.getJsonError(error.code_01003, error.desc_01003, db))
+                }
+            }).catch((err) => {
+                logger.error(req, cmd + '|Error:' + err)
+                logger.summary(req, cmd + '|' + error.desc_01002)
+                res.json(resp.getJsonError(error.code_01002, error.desc_01002, err))
+            })
+        } catch (err) {
+            logger.error(req, cmd + '|' + err)
+            return resp.getInternalError(req, res, cmd, err)
+        }
+    }
+}
+module.exports = vendorProfile
